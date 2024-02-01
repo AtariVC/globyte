9847c3a175ad02dc27171d9b9e4f2ce7431e9112
Vladislav Komarov <67183850+AtariVC@users.noreply.github.com>
2024-02-01 22:54:15 +0700

    123

--- main_app.py
+++ main_app.py
@@ -7,15 +7,4 @@ st.write("""
 Hello *world!*
 """)
 
-# Генерация случайных данных
-num_records = 100
-data = {
-    'RandomNumber1': [1, 2, 3],
-    'RandomNumber2': [1, 2, 3]
-}
 
-# Создание DataFrame
-df = pd.DataFrame(data)
-
-df = pd.read_csv('random_numbers.csv')
-st.line_chart(df)
